2d4b33136471efbbc89dd1c4cfde5574bcc93479
ajithkumar23k <ajithkumar_23@outlook.com>
2021-06-10 18:19:21 +0530

    django

---
+++ advcbv/basic_app/urls.py
@@ -0,0 +1,28 @@
+"""advcbv URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+
+from basic_app import views
+
+app_name = 'basic_app'
+urlpatterns = [
+
+    url(r'^$', views.SchoolListView.as_view(),name='list'),
+    url(r'^(?P<pk>\d+)/$',views.SchoolDetailView.as_view(),name='detail'),
+    url(r'^create/$', views.SchoolCreateView.as_view(), name='create'),
+    url(r'^update/(?P<pk>\d+)/$', views.SchoolUpdateView.as_view(), name='update'),
+    url(r'^delete/(?P<pk>\d+)/$', views.SchoolDeleteView.as_view(), name='delete'),
+]
